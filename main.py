fb9967911ef340e2dfbcfb3f3062b70075808a93
PirateAggro <152743903+PirateAggro@users.noreply.github.com>
2024-03-09 16:13:59 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,131 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime, timezone, timedelta, date
+from google.cloud import firestore
+
+import  retorn, historial, dadesmestres, materials, reserva_fb_Boot, reserva, app3
+
+st.set_page_config(
+    page_title="Gestió Reserves",
+    page_icon="👋",
+    layout="wide",
+    
+)
+
+# FUNCTIONS DEFINITION
+
+# # Function to read data from Google Sheet
+# def read_google_sheet_x(sheet_url):
+    
+#     # Use credentials to create a client to interact with Google Drive API
+#     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+#     credentials = ServiceAccountCredentials.from_json_keyfile_name("hola-407517-a0a85576df69.json", scope)
+#     gc = gspread.authorize(credentials)
+
+#     # Open the Google Sheet using its URL
+#     worksheet = gc.open_by_url(sheet_url).sheet1
+
+#     # Get the data as a Pandas DataFrame
+#     data = worksheet.get_all_records()
+#     df = pd.DataFrame(data)
+
+#     return df
+
+# MULTIPAGE MENU DEFINITION
+
+class Multiapp:
+    def __ini__(self):
+        self.apps = []
+    def add_app(self,title,funcion):
+        self.apps.append({
+            "title": title,
+            "function": function
+        })
+
+    # CHAT
+    def run():  
+
+        # Initialization
+        if 'key' not in st.session_state:
+            st.session_state['flag_fixed'] = ""
+        with st.sidebar:
+            app = option_menu(
+                menu_title='Menu',
+                options=["Title","Reserva", "Retorn", "Historial"],
+                menu_icon="cast",
+                default_index=0
+            )
+
+        if app == 'Title':
+            st.image('sunrise.jpeg', caption='Sunrise by the mountains')
+
+        if app == 'Reserva':
+            app3.app()
+
+        if app == 'Retorn':
+            retorn.app()
+
+        if app == 'Historial':
+            reserva.app()
+
+
+    '''
+    NOSTRA
+    def run():
+        with st.sidebar:
+            app = option_menu(
+                menu_title='Menu',
+                options=["Title","Reserva", "Retorn", "Historial"],
+                menu_icon="cast",
+                default_index=0
+            )
+        ##my_expander = st.expander(label='Expand me')
+        ##my_expander.write('Hello there!')
+        ##clicked = my_expander.button('Click me!')
+        if app=='Title':
+                    st.image('sunrise.jpeg', caption='Sunrise by the mountains')
+
+
+                    # LOAD PRODUCTS FROM GOOGLE SHEET
+                    #sheet_url = "https://docs.google.com/spreadsheets/d/10OJjKforD1t0VSG1ynpa5QlQ2WbvmXDCGz8R4yxkoXM/edit#gid=0"
+
+                    # if 'df_products' not in st.session_state:
+                    #     st.session_state.df_products = None
+                    # df_products = read_google_sheet_x(sheet_url)
+                    # st.session_state.df_products = df_products
+                    # st.write("Products loaded")
+
+
+
+                    # # LOAD CLIENTS FROM GOOGLE SHEET
+                    # sheet_url = "https://docs.google.com/spreadsheets/d/1uMANAvFf14030QZHner0incZyE2Tj9ex04Uiu1H-ldE/edit#gid=0"
+                    # if 'df_clients' not in st.session_state:
+                    #     st.session_state.df_clients = None
+                    # df_clients = read_google_sheet_x(sheet_url)
+                    # st.session_state.df_clients = df_clients
+                    # st.write("Clients loaded")
+
+
+                    # # LOAD BOOKINGS FROM GOOGLE SHEET
+                    # sheet_url = "https://docs.google.com/spreadsheets/d/16AbAcJcrp5RL-dEO5EjddgqJlwu9JUo-DjzS5tZlzUU/edit#gid=0"
+                    # if 'df_bookings' not in st.session_state:
+                    #     st.session_state.df_bookings = None
+
+                    # df_bookings = read_google_sheet_x(sheet_url)
+                    # st.session_state.df_bookings = df_bookings
+                    # #st.dataframe(df_bookings)
+                    # st.write("Bookings loaded")
+
+        if app=='Reserva':
+            app3.app()
+        if app=='Retorn':
+            retorn.app()
+        if app=='Historial':
+            reserva.app()
+    run()
+    '''
+    run()
---
+++ retorn.py
@@ -0,0 +1,158 @@
+
+import streamlit as st
+import pandas as pd
+import gspread
+import time
+from datetime import date
+from oauth2client.service_account import ServiceAccountCredentials
+from streamlit_option_menu import option_menu
+from datetime import datetime, timezone, timedelta, date
+from google.cloud import firestore
+
+
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+# Function to read data from Google Sheet
+def read_google_sheet_x(sheet_url):
+    
+    # Use credentials to create a client to interact with Google Drive API
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("hola-407517-a0a85576df69.json", scope)
+    gc = gspread.authorize(credentials)
+
+    # Open the Google Sheet using its URL
+    worksheet = gc.open_by_url(sheet_url).sheet1
+
+    # Get the data as a Pandas DataFrame
+    data = worksheet.get_all_records()
+    df = pd.DataFrame(data)
+
+    return df
+
+def write_google_sheet_x(index, docent, comentari):
+
+    #print(f'index a write: {index}')
+
+    sheet_url = "https://docs.google.com/spreadsheets/d/16AbAcJcrp5RL-dEO5EjddgqJlwu9JUo-DjzS5tZlzUU/edit?pli=1#gid=1859943936"
+
+    # Google Sheets credentials
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("hola-407517-a0a85576df69.json", scope)
+    gc = gspread.authorize(credentials)
+    sheet = gc.open_by_url(sheet_url).sheet1
+    
+    last_row = len(sheet.get_all_values())+1
+    #st.write(dt)
+    # Example datetime object
+    dt = datetime.now().strftime('%a %d %b %Y, %I:%M%p')
+    
+
+    for i in range(last_row):
+        if i == index :
+            sheet.update_cell(index,10, 'retornat' )  #estat       
+            sheet.update_cell(index,13, docent  )  #docent rep producte
+            sheet.update_cell(index,12, comentari )  #docent rep producte
+            sheet.update_cell(index,14, dt )  #docent rep producte
+            st.warning('Reserva retornada amb èxit', icon="👋")
+ 
+
+def app():
+
+    concat = ""
+    unique_clients_code = []
+    unique_clients_code.append(" ")
+
+    unique_docents_code = []
+    unique_docents_code.append(" ")
+
+    unique_clients_name = []
+    unique_clients_name.append(" ")
+
+    selected_codi = ""
+    ambit = ""
+
+    # LOAD CLIENTS FROM GOOGLE SHEET
+    sheet_url = "https://docs.google.com/spreadsheets/d/1uMANAvFf14030QZHner0incZyE2Tj9ex04Uiu1H-ldE/edit#gid=0"
+    if 'df_clients' not in st.session_state:
+        st.session_state.df_clients = None
+    df_clients = read_google_sheet_x(sheet_url)
+    #st.session_state.df_clients = df_clients
+    #st.write("Clients loaded")
+    
+    for index, row in df_clients.iterrows():
+        for column in df_clients.columns:
+            
+            if column == "Nom":
+                nom = row[column]
+                #st.write(f"Element at index {index} and column {column}: {row[column]}")
+                #st.write(nom)
+            if column == "Alumne":
+                alumne = row[column]
+                #st.write(f"Element at index {index} and column {column}: {row[column]}")
+                #st.write(alumne)
+            if column == "AP":
+                ambit =row[column]
+                
+        concat = str(alumne) + " - "+ nom
+        if ambit == "P":
+            concatp = str(alumne) + " - "+ nom
+            unique_docents_code.append(concatp)
+        #st.write(concat)
+        unique_clients_code.append(concat)
+
+    unique_docents_code.sort
+    selected_codi_docent = st.selectbox('Personal Docent que recepciona material:', unique_docents_code)
+    if selected_codi_docent != " ":
+        docent = selected_codi_docent
+        # st.write("Escollit", selected_codi_docent)   
+        # if selected_codi > 0:
+        separator = " - "
+        part = selected_codi_docent.split(separator)
+        # st.write(" Part: ", part)
+        # st.write(part[0])
+        selected_codi_docent = int(part [0])        
+        # st.write("Escollit codi", selected_codi_docent)  
+
+
+    unique_clients_code.sort
+    selected_codi_reservador = st.selectbox('Persona que retorna material:', unique_clients_code)
+    if selected_codi_reservador != " ":
+        # st.write("Escollit", selected_codi_reservador)
+        # if selected_codi > 0:
+        separator = " - "
+        part = selected_codi_reservador.split(separator)
+        # st.write(" Part: ", part)
+        # st.write(part[0])
+        selected_codi_reservador = int(part [0])        
+        # st.write("Escollit codi", selected_codi_reservador)  
+
+
+            #df_reserves = db.collection("Reserves")
+            #query = db.collection('Reserves').where('Client', '==', selected_codi_reservador)
+
+            # LOAD RESERVES FROM GOOGLE SHEET
+
+    sheet_url = "https://docs.google.com/spreadsheets/d/16AbAcJcrp5RL-dEO5EjddgqJlwu9JUo-DjzS5tZlzUU/edit#gid=0"
+    if 'df_reserves' not in st.session_state:
+        st.session_state.df_reserves = None
+
+    df_reserves = read_google_sheet_x(sheet_url)
+    #st.write(df_reserves)
+
+    filtered_df_prod = df_reserves[df_reserves['client']==selected_codi_reservador]
+
+
+    for row in filtered_df_prod:
+        filtered_df_prod['producte retornat'] = False
+        filtered_df_prod['estat retorn'] = " "
+    edited_filtered_df_prod = st.data_editor(filtered_df_prod, column_order=("producte retornat","estat retorn", "client", "nom","material", "data_inici", "data_fi"))
+    
+    if st.button('RETORN'):
+        for index, row in edited_filtered_df_prod.iterrows():  # index 0 i 1  
+            if row['producte retornat'] == True:
+                #print(f'index : {index}')
+                #print(f'Line 405: edited df: {reserva_df}')
+                write_google_sheet_x(index+2, docent, row['estat retorn'])
+      
+    
